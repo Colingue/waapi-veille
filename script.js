9da1305f7582ea35cc71b63504252ed65570a44c
Colin NOIRET <Colin.NOIRET@etu.uca.fr>
2022-01-23 21:09:22 +0100

    Maj exemple

--- script.js
+++ script.js
@@ -1,44 +1,37 @@
-// let layerCount = 4;
-// let starCount = 500;
-// let maxTime = 31;
-//
-// let universe = document.getElementById('universe');
-//
-// let width = universe.style.width;
-// let height = universe.style.height;
-//
-// for (let i = 0; i < starCount; i++){
-//     let ypos = Math.round(Math.random() * height);
-//     let star = document.createElement('div');
-//     let speed = 1000 * (Math.random() * maxTime * height);
-//     // star.setAttribute('class', 'star' + (3 - Math.floor((speed / 1000 ) / 8)));
-//     star.setAttribute('class', 'star');
-//     star.style.backgroundColor = 'white';
-//     universe.appendChild(star);
-//     star.animate([
-//         {transform: 'translate3d(' + width + 'px, ' + ypos + 'px, 0)'},
-//         {transform: 'translate3d(-' + Math.random() * 256 + 'px, ' + ypos + 'px, 0)'}
-//     ],
-//         {delay: Math.random() * -speed, duration: speed, iterations: 1000})
-// }
+const starCount = 500;
 
-let starCount = 500;
-let maxTime = 31;
-let universe = document.getElementById('universe');
-let w = window;
-let d = document;
-let e = d.documentElement;
-let g = d.getElementsByTagName('#universe')[0];
-let width = 600;
-let height = 600;
+const maxTime = 31;
+
+const width = 600;
+
+const height = 600;
+
+const universe = document.getElementById('universe');
+
 for (let i = 0; i < starCount; ++i) {
+
     let ypos = Math.round(Math.random() * height);
+
     let star = document.createElement('div');
+
     let speed = 1000 * (Math.random() * maxTime + 1);
-    star.setAttribute('class', 'star'); // + (3 - Math.floor((speed / 1000) / 8)));
-    star.style.backgroundColor = 'white';//rgb(' + Math.round(Math.random() * 255) + ', ' + Math.round(Math.random() * 255) + ', ' + Math.round(Math.random() * 255) + ')';
+
+    star.setAttribute('class', 'star');
+
     universe.appendChild(star);
+
     star.animate([{transform: 'translate3d(' + width + 'px, ' + ypos + 'px, 0)'},
-            {transform: 'translate3d(-' + Math.random() * 256 + 'px, ' + ypos + 'px, 0)'}],
-        {delay: Math.random() * -speed, duration: speed, iterations: 1000});
+
+            {transform: 'translate3d(-' + 0 + 'px, ' + ypos + 'px, 0)'}],
+
+        {
+
+            delay: Math.random() * -speed,
+
+            duration: speed,
+
+            iterations: 1000}
+
+    );
+
 }
